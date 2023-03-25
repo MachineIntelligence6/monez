4a01c68d48175e92d1b3e954794474cca3b0d96f
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2023-03-25 10:46:32 +0500

    Update Add Feed Page

--- resources/views/feeds/form.blade.php
+++ resources/views/feeds/form.blade.php
@@ -5,7 +5,7 @@
 <div>
     <h5 class="mb-3 text-uppercase">Feed Info</h5>
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-7">
             <div class="mb-3">
                 <label for="feedName" class="form-label">Feed Name</label>
                 <input type="text" class="form-control" id="feedName" name="feedName" placeholder="Enter Feed Name" />
@@ -15,7 +15,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-7">
             <div class="mb-3">
                 <label for="feedUrl" class="form-label">Feed Url</label><label class="text-danger">*</label>
                 <input type="url" class="form-control" id="feedUrl" name="feedUrl" placeholder="Enter Feed Url" required>
@@ -25,36 +25,36 @@
                 </div>
             </div>
         </div> <!-- end col -->
-        <div class="col-md-4">
+        <div class="col-md-7">
             <div class="mb-3">
                 <label for="keywordParameter" class="form-label">Keyword Parameter</label><label class="text-danger">*</label>
                 <input type="text" class="form-control" id="keywordParameter" name="keywordParameter" placeholder="Enter Keyword Parameter" required>
             </div>
         </div>
     </div> <!-- end row -->
-    <div class="row justify-content-between px-2">
+    <div class="col-md-7 my-2 row justify-content-between p-0 pl-2">
         <h5 class="text-uppercase">Feed Url Parameters</h5>
         <button type="button" class="btn btn-secondary"><i class="mdi mdi-plus"></i></button>
     </div>
-    <div class="row">
-        <div class="col-md-6 mb-3">
-            <label for="feedParamName" class="form-label">Parameter Name</label>
-            <input type="text" class="form-control" id="feedParamName" name="feedParamName" placeholder="Enter Parameter Name" />
-            <div class="valid-feedback">Valid.</div>
-            <div class="invalid-feedback">
-                You must enter valid input
+    <div id="feedUrlParametersContainer">
+        <div class="row mb-3 feedUrlParameter">
+            <div class="col-md-7 mb-1">
+                <input type="text" class="form-control" id="feedParamName" name="feedParamName" placeholder="Enter Parameter Name" />
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">
+                    You must enter valid input
+                </div>
             </div>
-        </div>
-        <div class="col-md-6 mb-3">
-            <label for="feedParamValue" class="form-label">Parameter Value</label>
-            <input type="text" class="form-control" id="feedParamValue" name="feedParamValue" placeholder="Enter Parameter Value" />
-            <div class="valid-feedback">Valid.</div>
-            <div class="invalid-feedback">
-                You must enter valid input
+            <div class="col-md-7 mb-1">
+                <input type="text" class="form-control" id="feedParamValue" name="feedParamValue" placeholder="Enter Parameter Value" />
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">
+                    You must enter valid input
+                </div>
             </div>
         </div>
     </div>
-    
+
     <div class="row pl-2">
         <button class="btn btn-primary col-auto" type="submit">Add Feed</button>
         <a href="{{ route('feeds.index') }}" class="btn btn-secondary ml-1">Cancel</a>
@@ -72,6 +72,11 @@
 <script src="{{asset('assets/js/pages/form-validation.init.js')}}"></script>
 <script src="{{asset('assets/js/pages/form-advanced.init.js')}}"></script>
 <script>
+
+    const feedUrlParametersContainer = document.getElementById("feedUrlParametersContainer");
+
+    function addFeedUrlParameter()
+
 </script>
 
 @endsection
