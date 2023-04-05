696b48601d08d938966501116178ccc76d3a31a5
Aslam Hussain <aslamhussain.cs@gmail.com>
2023-04-05 13:38:32 +0500

    Revenue Report Page

--- resources/views/reports/revenue.blade.php
+++ resources/views/reports/revenue.blade.php
@@ -27,6 +27,39 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
+                    <div class="row mb-3 justify-content-end">
+                        <div class="col-auto">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile" accept=".csv">
+                                        <label class="btn btn-secondary" for="customFile">Upload CSV</label>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-primary" data-trigger="modal" data-target="apiDetailModal">API Details</button>
+                                </div>
+                                <div class="col-auto" style="min-width: 160px">
+                                    <select class="form-control" name="" data-toggle="select2" required>
+                                        <option>Show Columns</option>
+                                        <option value="">Latency (Seconds)</option>
+                                        <option value="">Follow On Searches (%)</option>
+                                        <option value="">Coverage (%)</option>
+                                        <option value="">CTR (%)</option>
+                                        <option value="">RPM ($)</option>
+                                        <option value="">Monetized RPM (%)</option>
+                                        <option value="">EPC ($)</option>
+                                    </select>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile" accept=".csv">
+                                        <label class="btn btn-secondary" for="customFile">Export CSV</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="row mb-2 align-items-center justify-content-between">
                         <div class="col-auto">
                             <label>
@@ -40,7 +73,7 @@
                                 entries
                             </label>
                         </div>
-                        <div class="col-7">
+                        <div class="col-6">
                             <div class="row">
                                 <div class="col-3">
                                     <select class="form-control" name="parteners" id="select-partners" data-toggle="select2">
@@ -60,7 +93,7 @@
                                         <option value="select-custom">Select Custom</option>
                                     </select>
                                 </div>
-                                <div class="col-auto" style="min-width: 200px;">
+                                <div class="col-auto" style="min-width:160px;">
                                     <select class="form-control selectperiod" name="" data-toggle="select2" required>
                                         <option>Select Period</option>
                                         <option value="">Month to Date</option>
@@ -69,19 +102,18 @@
                                     </select>
                                     <input type="text" id="range-datepicker" style="width: 0; height: 0; overflow: hidden;" class="form-control border-0 p-0 custom-range-date-picker" placeholder="Start Date to End Date">
                                 </div>
+                                <div class="col-auto">
+                                    <div class="pl-2 d-flex align-items-center h-100 countrywise__checkbox">
+                                        <input class="form-check-input mt-0" type="checkbox" value="" id="countrywise">
+                                        <label class="form-check-label" for="countrywise">
+                                            Country Wise
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-2">
-                            <select class="form-control" name="" data-toggle="select2" required>
-                                <option>Show Columns</option>
-                                <option value="">Latency (Seconds)</option>
-                                <option value="">Follow On Searches (%)</option>
-                                <option value="">Coverage (%)</option>
-                                <option value="">CTR (%)</option>
-                                <option value="">RPM ($)</option>
-                                <option value="">Monetized RPM (%)</option>
-                                <option value="">EPC ($)</option>
-                            </select>
+                        <div class="col-auto">
+                            <button class="btn btn-primary">Go</button>
                         </div>
                     </div>
                     <div class="table-responsive">
@@ -146,10 +178,10 @@
     <!-- end row -->
 </div> <!-- container -->
 
-
+@include('reports.modals.apidetail-modal')
 @endsection
 
-@section('script')
+@section('script-bottom')
 <script src="{{asset('assets/libs/select2/select2.min.js')}}"></script>
 <script src="{{asset('assets/libs/datatables/datatables.min.js')}}"></script>
 
@@ -164,7 +196,7 @@
 <!-- <script src="{{asset('assets/libs/sumomultiselect/sumoselect.min.css')}}"></script> -->
 <!-- Page js-->
 <script src="{{asset('assets/js/pages/form-pickers.init.js')}}"></script>
-
+<script src="{{asset('assets/js/modal-init.js')}}"></script>
 <script type="text/javascript">
     $('#products-datatable').DataTable({
         searching: false,
