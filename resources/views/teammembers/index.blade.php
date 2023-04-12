3a003b52380da000542ca5003f6efd8d82fd4e7a
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2023-04-12 10:32:15 +0500

    Update index.blade.php

--- resources/views/teammembers/index.blade.php
+++ resources/views/teammembers/index.blade.php
@@ -47,7 +47,7 @@
                                         </div>
                                     </th>
                                     <th>Name</th>
-                                    <th>Email</th>
+                                    <th style="width: 100%;">Email</th>
                                     <th>Actions</th>
                                 </tr>
                             </thead>
@@ -62,23 +62,23 @@
                                         </div>
                                     </td>
                                     <td>{{ $teamMember->name }}</td>
-                                    <td>{{ $teamMember->email }}</td>
+                                    <td style="width: 100%;">{{ $teamMember->email }}</td>
                                     <td>
                                         <span class="d-inline-flex" style="gap: 5px;">
-                                        <a class="btn bg-secondary text-white" href="{{route('team-members.view',$teamMember->id)}}">View Info</a>
-                                        @if(isset($teamMember->advertisers))
-                                        @else
-                                        <form action="{{ route('team-members.destroy', $teamMember->id )  }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Delete</button>
-                                        </form>
-                                        @endif
+                                            <a class="btn bg-secondary text-white" href="{{route('team-members.view',$teamMember->id)}}">View Info</a>
+                                            @if(isset($teamMember->advertisers))
+                                            @else
+                                            <form action="{{ route('team-members.destroy', $teamMember->id )  }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                            @endif
                                         </span>
                                     </td>
 
 
-                                   
+
                                 </tr>
                                 @endforeach
                                 @endif
@@ -98,7 +98,7 @@
 
 <script type="text/javascript">
     $('#products-datatable').DataTable({
-        
+
     });
 </script>
 <script>
