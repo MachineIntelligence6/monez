0ffa179efe3f3b796b26af6c1064008a9fffcd47
Hamza Saqib <mianhamza7262@gmail.com>
2023-05-05 11:37:39 +0500

    country seeder

--- database/seeds/CountrySeeder.php
+++ database/seeds/CountrySeeder.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Database\Seeders;
-
 use App\Country;
 use Illuminate\Database\Seeder;
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(BankSeeder::class);
-		// $this->call(CountrySeeder::class);
+		$this->call(CountrySeeder::class);
 		$this->call(UserSeeder::class);
 
         TeamMember::create([
