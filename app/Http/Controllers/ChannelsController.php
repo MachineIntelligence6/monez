1ee23e3b34ba55aacad4ab610936f418985dd032
AmnaNabeel <148874165+AmnaNabeel@users.noreply.github.com>
2023-11-07 20:00:07 +0500

    Update ChannelsController.php

--- app/Http/Controllers/ChannelsController.php
+++ app/Http/Controllers/ChannelsController.php
@@ -465,23 +465,34 @@ class ChannelsController extends Controller
 
             if ($isQueriesValid) {
                 $device = Agent::device();
-                if($device == 'webkit')
+                $platform = Agent::platform();
+                
+                if(strtolower($device) == 'webkit')
                 {
                     $device = 'Laptop/Desktop';
                 }
-                elseif($device == '')
+                elseif(strtolower($device) == 'macintosh')
                 {
-                    $device = 'Mobile Phone';
+                    $device = 'Macbook';
                 }
                 else{
                 $device = 'Unknown Device';
                 }
+
+                if(strtolower($platform) == 'ios')
+                {
+                    $platform = 'Iphone';
+                }
+                // elseif($platform == 'Android ')
+                // {
+                //     $platform = 'Android';
+                // }
                 $channelSearch = ChannelSearch::create([
                     'channel_id' => isset($cahnnel) ? $cahnnel->id : null,
                     'ip_address' => $ip,
                     'browser' => Agent::browser(),
                     'device' => $device,
-                    'os' => Agent::platform(),
+                    'os' => $platform,
                     'user_agent' => $request->userAgent(),
                     'feed_id' => isset($feed) ? $feed->id : 1,
                     'feed' => isset($feed) ? $feed->feedId : 'F1_fallback',
