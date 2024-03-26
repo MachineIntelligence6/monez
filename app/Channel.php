ca571562171c2995c99bea7c42f0f38c4b667b1e
Ahmed Ali <ahmedali5530@gmail.com>
2024-03-26 21:26:25 +0300

    fixed feeds status when disabling channel

--- app/Channel.php
+++ app/Channel.php
@@ -27,7 +27,7 @@ class Channel extends Model
     public function makeFeedAvailable()
     {
         foreach ($this->feeds() as $feed) {
-            if (Channel::where('feed_ids',$feed->id)->where('is_active', true)->exists()) {
+            if (Channel::where('feed_ids', 'like', '%'.$feed->id.'%')->where('is_active', true)->exists()) {
                 DB::table('feeds')->where('id', $feed->id)->update(['status' => 'available']);
             }
         }
